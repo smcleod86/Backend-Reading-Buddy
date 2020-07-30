2bef55171469879e66d24c163886858b3e1855aa
smcleod86 <mcleod.scott_86@yahoo.com>
2020-07-30 15:34:02 -0700

    setting up uri server

--- server.js
+++ server.js
@@ -25,9 +25,17 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 //connect mongo db
-const db = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI
+const MongoClient = require('mongodb').MongoClient;
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 //connect Mongodb
-mongoose.connect(db)
+mongoose.connect(uri)
     .then(() => console.log('MongoDB connected... ✅'))
     .catch(err => console.log(err))
 
